80d368568a09e375267953343eeac809c32990ae
Mugisha Regis <mugisharegis72@gmail.com>
2023-11-25 22:13:56 +0200

    added tabs

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -11,8 +11,12 @@ import { View,
 import styles from './welcome.style'
 import { icons, SIZES} from '../../../constants'
 
+const jobTypes = ["Full-Time", "Part-Time" , "Contract", "Professional" , "Per-Hour"]
+
 const Welcome = () => {
   const router = useRouter();
+  const [activeJobType , setActiveJobType] = useState("Full-Time")
+
   return (
     <View>
       <View style={styles.container}>
@@ -23,8 +27,37 @@ const Welcome = () => {
         <View style={styles.searchWrapper}>
           <TextInput 
           style={styles.searchInput}
+          value=''
+          onChange={() => {}}
+          placeholder='What are you looking for ?'
           />
         </View>
+          <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+            <Image 
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+            />
+          </TouchableOpacity>
+      </View>
+      <View style={styles.tabsContainer}>
+        <FlatList 
+        data={jobTypes}
+        renderItem={({item}) => (
+          <TouchableOpacity
+          style={styles.tab(activeJobType, item)}
+          onPress={() => {
+            setActiveJobType(item);
+            router.push(`/search/${item}`)
+          }}
+          >
+            <Text style={styles.tabText(activeJobType , item)}>{item}</Text>
+          </TouchableOpacity>
+        )}
+        keyExtractor={item => item}
+        contentContainerStyle={{ columnGap: SIZES.small}}
+        horizontal
+        />
       </View>
     </View>
   )

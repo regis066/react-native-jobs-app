00383f23f925abba0943dd35c87477f48bfc812b
Mugisha Regis <mugisharegis72@gmail.com>
2023-11-26 22:08:29 +0200

    intergrating with rapid API

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -4,11 +4,15 @@ import { useRouter } from "expo-router";
 import styles from './popularjobs.style'
 import {COLORS , SIZES} from "../../../constants"
 import PopularJobCard from '../../common/cards/popular/PopularJobCard'
+import useFetch from '../../../hook/useFetch';
 
 const Popularjobs = () => {
   const router = useRouter();
-  const isLoading  = true;
-  const error = false;
+  const { data , isLoading, error} = useFetch('search' , 
+  {
+    query: "React developer",
+    num_pages: 1
+  })
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -24,7 +28,15 @@ const Popularjobs = () => {
         ) : error ? (
           <Text>Something went wrong</Text>
         ): (
-          <FlatList />
+          <FlatList 
+          data={[1,2,3,4]}
+          renderItem={({item}) => (
+            <PopularJobCard item={item} />
+          )}
+          keyExtractor={item => item?.job_id}
+          contentContainerStyle={{ columnGap: SIZES.medium}}
+          horizontal
+          />
         )}
       </View>
     </View>
